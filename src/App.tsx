a34317f2c2d980a0678ce2a0983b3b4d6502690f
IasmimCristina <iaasmimcristinaa@gmail.com>
2024-11-30 13:13:53 -0300

    testing

--- src/App.tsx
+++ src/App.tsx
@@ -6,12 +6,13 @@ import './styles/global.css';
 import './App.css';
 import { FilterProvider } from './contexts/FilterProvider';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 const App: React.FC = () => {
 
   const queryClient = new QueryClient();
   return (
     <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false} />
       <PokemonDataProvider>
         <FilterProvider>
           <Header />
